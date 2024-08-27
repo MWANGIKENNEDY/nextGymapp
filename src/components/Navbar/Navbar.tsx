c5092320cc8a637c47347dc5dc463295c5eaaa13
MWANGIKENNEDY <kenmwan75@gmail.com>
2024-08-27 11:04:22 +0300

    ft:added mobile navbar with framer-motion

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import { FaSearch } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { CiShoppingCart } from "react-icons/ci";
+
 import { FaDumbbell } from "react-icons/fa";
+import { navMenu } from "./navMenu";
+import Link from "next/link";
+import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="container">
+    <nav className="">
+      <div className="container flex items-center justify-between h-20  relative">
         {/* logo section */}
 
         <div className="text-2xl flex items-center gap-2 font-bold uppercase">
@@ -18,7 +23,28 @@ const Navbar = () => {
 
         {/* menu section  */}
 
+        <div className="hidden xl:flex items-center gap-4 justify-between ">
+          {navMenu.map((nav) => (
+            <Link className=" hover:text-primary duration-200" href={nav.href}>{nav.title}</Link>
+          ))}
+        </div>
+
+        {/* icons section */}
+
+        <div className=" flex items-center justify-between gap-4">
+          <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
+            <FaSearch></FaSearch>
+          </button>
+          <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
+            <CiShoppingCart></CiShoppingCart>
+          </button>
+
+          <button className=" hover:bg-primary text-primary font-semibold hover:text-white rounded-md border-2 border-primary px-6 py-2 duration-200 hidden xl:block">Login</button>
+        </div>
+
         {/* mobile hamberger section */}
+
+<MobileMenu/>
       </div>
     </nav>
   );
