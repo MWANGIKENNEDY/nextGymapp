4e89762b8af6371bc913c60b0912684843e32e94
MWANGIKENNEDY <kenmwan75@gmail.com>
2024-08-28 07:45:31 +0300

    fx:added key prop for array.map

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import React from "react";
+
+import { FaPlay } from "react-icons/fa";
+
+const Hero = () => {
+  return (
+    <section>
+      <div className=" container grid min-h-[600px] grid-cols-1 md:grid-cols-2 relative">
+        <div className="flex flex-col justify-center py-20 md:py-0">
+          <div className="space-y-6 text-center md:text-left">
+            <h1 className="text-5xl font-bold leading-relaxed lg:text-6xl xl:leading-normal">
+              Gym Gives you the perfect Health
+            </h1>
+
+            <p className="text-gray-500 xl:max-w-6xl">
+              It is a long established fact that a reader will be by readable
+              content of a page when are the best product.
+            </p>
+
+            <div className="button-section !mt-4 flex justify-center gap-8 md:justify-start">
+              <button className="primary-btn flex items-center gap-2">
+                {" "}
+                Order Now
+              </button>
+              <button className="flex items-center justify-center gap-2">
+                <FaPlay/>Play Now
+              </button>
+            </div>
+          </div>
+        </div>
+      
+        <div className=" relative flex items-center justify-center min-h-[300px]  ">
+          <Image alt="" src="/gym1.png" layout="fill" objectFit="contain" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
