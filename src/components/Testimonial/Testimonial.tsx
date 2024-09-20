693fb2080571827ac8adad9957461127507330d8
MWANGIKENNEDY <kenmwan75@gmail.com>
2024-09-20 07:09:48 +0300

    fx:added equipment section with framer-motion animations

--- src/components/Testimonial/Testimonial.tsx
+++ src/components/Testimonial/Testimonial.tsx
@@ -60,28 +60,29 @@ const Testimonial = () => {
     pauseOnFocus: true,
     responsive: [
       {
-        breakpoint: 10000,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-        },
-      },
-      {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: 2,
-        },
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
       },
       {
-        breakpoint: 640,
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
+          slidesToScroll: 1
+        }
+      }
     ],
   };
 
